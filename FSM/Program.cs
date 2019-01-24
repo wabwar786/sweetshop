fca236d94b29f33ebffa058188e4083cb9926ffa
wabwar <wabwar786@gmail.com>
2019-01-24 14:16:55 +0500

    myfirst commet

--- FSM/Program.cs
+++ FSM/Program.cs
@@ -16,7 +16,7 @@ namespace FSM
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new mainfrm());
         }
     }
 }
